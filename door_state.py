4565c466172c5ed86e9500f5bc12e42ecfc5a591
mijaki <85630936+mijaki@users.noreply.github.com>
2021-06-18 13:07:19 -0400

    First working version

--- door_state.py
+++ door_state.py
@@ -1,16 +1,50 @@
+from datetime import datetime
 from enum import Enum
+
+import options
 
 DOOR_STATE = {}
 
+
 class State(Enum):
-    CLOSED=0
-    OPEN=1
+    CLOSED = 0
+    OPEN = 1
+    UNKNOWN = 2
+
+class StateTime:
+    def __init__(self, state, timestamp):
+        self.state = state
+        self.timestamp = timestamp
+        self.last_warning_timestamp = timestamp
 
 
-def update_door_state(door_name, state):
+def update_door_state(door_name, string_state):
+    state=State.UNKNOWN
+    if string_state=='Opened':
+        state=State.OPEN
+    elif string_state == 'Closed':
+        state = State.CLOSED
+
     if door_name in DOOR_STATE:
-        print("updating '" + door_name + "' from " + DOOR_STATE[door_name] + " to " + state)
-        DOOR_STATE[door_name] = state
+        print("updating '" + door_name + "' from " + str(DOOR_STATE[door_name].state) + " to " + str(state))
     else:
-        DOOR_STATE[door_name] = state
-        print("created '" + door_name + "' as " + state)
+        print("created '" + door_name + "' as " + str(state))
+    DOOR_STATE[door_name] = StateTime(state, datetime.now())
+
+
+def validate_door_states():
+    # return a list of doors that have been open too long
+    open_doors = []
+    for name, door in DOOR_STATE.items():
+        if door.state == State.OPEN:
+            difference = (datetime.now() - door.timestamp)
+            seconds_door_was_open = difference.total_seconds()
+            if seconds_door_was_open >= options.INITIAL_WARNING_THRESHOLD:
+                difference = (datetime.now() - door.last_warning_timestamp)
+                seconds_since_last_warning = difference.total_seconds()
+                if seconds_since_last_warning >= options.NEXT_WARNING_THRESHOLD:
+                    open_doors.append(name)
+                    door.last_warning_timestamp = datetime.now()
+    if open_doors:
+        print("Found these doors to be open too long: " + str(open_doors))
+    return open_doors
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import io
 import json
+import time
+
 import requests
 
+import door_state
 import gmail_checker
 import options
 
@@ -10,13 +13,39 @@ settings = {}
 with io.open('settings.json', "r", encoding="utf-8") as json_file:
     settings = json.load(json_file)
 
-def announce_door_open(door_name):
-    announcement = 'The ' + door_name + ' door is open'
-    announcement = announcement.replace(' ', '%20')
+
+def announce(announcement):
     if options.ANNOUNCEMENTS_ENABLED:
-        response = requests.get(settings['announcement_url']+announcement)
-        print(response)
+        response = requests.get(settings['announcement_url'] + announcement.replace(' ', '%20'))
+        if not response.ok:
+            print(response)
     else:
         print("Would be announcing: " + announcement)
 
-gmail_checker.main()
+
+def process_notifications():
+    notifications = gmail_checker.check_notifications()
+    for notification in notifications:
+        # TODO switch to logging to a file
+        print("Processing: " + notification)
+        tokenized = notification.split(" ")
+        door_name = tokenized[2:len(tokenized) - 1]
+        door_name = ' '.join(door_name)
+        door_name = door_name.replace(" Door", "")
+        state = tokenized[len(tokenized) - 1]
+        door_state.update_door_state(door_name, state)
+        # FIXME remove the line below
+        announce('The ' + door_name + ' door is ' + state)
+
+
+def announce_doors_to_close():
+    doors_to_close = door_state.validate_door_states()
+    # TODO This will do multiple announcements if multiple doors are open, concatenate instead of loop
+    for door_name in doors_to_close:
+        announce('The ' + door_name + ' door is open')
+
+
+while True:
+    process_notifications()
+    announce_doors_to_close()
+    time.sleep(options.NOTIFICATION_CHECK_FREQUENCY)
